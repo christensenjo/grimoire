80ee5ee110f166c218a5b81031ab73138c0d165c
Joel Christensen <christensenjo14@gmail.com>
2023-12-31 13:51:11 -0700

    Fix footer location by flex justify-between-ing the app.vue contents

--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@ const colorMode = useColorMode();
 </script>
 
 <template>
-  <div class="bg-parchment-50 dark:bg-fortress h-screen w-screen mx-0 px-0">
+  <div class="bg-parchment-50 dark:bg-fortress h-screen w-screen mx-0 px-0 flex flex-col justify-between">
     <NavBar
       title="Grimoire"
     />
