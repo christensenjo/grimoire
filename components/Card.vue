66599ba6cefd9bf318d42e9a40804c77ea773813
Joel Christensen <christensenjo14@gmail.com>
2024-01-02 21:04:01 -0700

    Add cards to highlight features

---
+++ components/Card.vue
@@ -0,0 +1,56 @@
+<script setup>
+    const props = defineProps({
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        icon: {
+            type: String,
+            required: false,
+        },
+        image_path: {
+            type: String,
+            required: false,
+        },
+        showLink: {
+            type: Boolean,
+            required: false,
+            default: true,
+        },
+        link: {
+            type: String,
+            required: false,
+            default: '',
+        },
+        linkText: {
+            type: String,
+            required: false,
+        },
+        disclaimer: {
+            type: String,
+            required: false,
+        },
+    });
+</script>
+
+<template>
+    <div class="max-w-sm p-6 bg-parchment-100 border border-gray-200 rounded-lg shadow dark:bg-tome dark:border-gray-700 flex flex-col items-center">
+        <Icon v-if="icon" :name="icon" class="w-10 h-10 text-scroll dark:text-parchment" />
+        <img v-if="image_path" :src="'/img/' + image_path" class="w-10 h-8 fill-scroll dark:fill-red" />
+        <NuxtLink to="" class="flex flex-row gap-2 items-center">
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-fortress dark:text-parchment-50">{{ title }}</h5>
+            <p class="text-scroll text-xs">{{ disclaimer }}</p>
+        </NuxtLink>
+        <p class="mb-3 font-normal text-gray-500 dark:text-parchment">{{ description }}</p>
+        <NuxtLink v-if="linkText" :to="link" class="inline-flex items-center text-blue-600 hover:underline">
+            {{ linkText }}
+            <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+            </svg>
+        </NuxtLink>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="bg-parchment-50 dark:bg-fortress pb-32">
-        <div class="grid max-w-screen-xl px-4 pt-8 pb-2 mx-auto lg:gap-8 xl:gap-0 lg:pt-16 lg:pb-0 lg:grid-cols-12">
+        <div class="grid max-w-screen-xl px-4 pt-8 pb-2 mx-auto lg:gap-8 xl:gap-0 lg:pt-16 lg:pb-0 lg:grid-cols-12 mb-24">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-fortress dark:text-parchment">What lore lies within the pages of your Grimoire?</h1>
                 <p class="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-fortress-600 dark:text-gray-400">Use a universal worldbuilding tool to elevate your storytelling. Tailored for the imaginative mind.</p>
@@ -16,7 +16,48 @@
                 <img src="~/assets/img/hero/hero_sparkle_tome.svg" alt="Open Grimoire">
             </div>
         </div>
-        <div class="w-full">
+        <div class="w-fit max-w-screen-xl mx-4 lg:mx-auto text-center flex-col gap-3">
+            <h2 class="text-5xl font-bold text-fortress dark:text-parchment mb-6">A universal worldbuilding tool</h2>
+            <p class="text-2xl text-fortress-600 dark:text-parchment mb-8">Create unique settings, locations, and characters. Manage all your fantasy assets in a single modern application.</p>
+            <div class="flex flex-wrap gap-4 justify-center">
+                <Card 
+                    title="An Arcane Armory"
+                    description="Manage all of your worldbuilding assets from a single thematic tool. Organize your settings with locations, characters, and more."
+                    icon="i-mdi-treasure-chest"
+                />
+                <Card 
+                    title="Build Legends"
+                    description="Craft heroes, villains, or simple townsfolk. Use mechanically flexible and narrative rich tools."
+                    icon="i-clarity-users-solid"
+                />
+                <Card 
+                    title="Tome Trading"
+                    description="Browse the Grimoires of fellow creators and summon their shared assets to your aid."
+                    icon="i-material-symbols-book-2"
+                    disclaimer="*Coming Soon*"
+                />
+                <Card 
+                    title="Scribe's Choice"
+                    description="Dive deep into manual creation or let the currents of inspiration guide your hand through artificial intelligence."
+                    icon="i-fa6-solid-pen-nib"
+                    disclaimer="*Coming Soon*"
+                />
+                <Card 
+                    title="Dynamic Dialogue"
+                    description="Engage with your created characters through the magic of AI and unearth their stories."
+                    icon="i-majesticons-messages"
+                    disclaimer="*Coming Soon*"
+                />
+                <Card 
+                    title="Sorcerer's Studio"
+                    description="Craft immersive settings with presentation tools for artist attributed or AI generated fantasy art."
+                    icon="i-fa6-solid-palette"
+                    disclaimer="*Coming Soon*"
+                />
+            </div>
+        </div>
+    
+        <div class="w-full mb-32">
             <FeaturedCard />
             <FeaturedCard />
             <FeaturedCard />
