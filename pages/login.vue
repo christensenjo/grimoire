41789a61121c74032b7b328701fca1ad47fdb71d
Joel Christensen <christensenjo14@gmail.com>
2024-02-02 22:35:55 -0700

    Add login and sign up page, get initial supabase auth methods working

--- pages/login.vue
+++ pages/login.vue
@@ -9,15 +9,17 @@
 
 
     // METHODS
-    const signUp = async () => {
-        const { user, error } = await supabase.auth.signUp({
+    const signUp = async (event: { preventDefault: () => void; }) => {
+        event?.preventDefault()
+
+        const { data, error } = await supabase.auth.signUp({
             email: email.value,
             password: password.value,
-        });
+        })
 
-        console.log(user);
+        console.log(data)
 
-        if (error) console.log(error);
+        if (error) console.log(error)
     };
 
     const signIn = async () => {
@@ -48,16 +50,16 @@
             <form class="space-y-4 md:space-y-6" action="#">
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-fortress">Your email</label>
-                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="someone@example.com" required="true">
+                    <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fortress-900 dark:focus:border-blue-500" placeholder="someone@example.com" required="true">
                 </div>
                 <div>
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-fortress">Password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-parchment-50 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="true">
+                    <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-parchment-50 dark:focus:ring-fortress-900 dark:focus:border-blue-500" required="true">
                 </div>
                 <div class="flex items-center justify-between">
                     <div class="flex items-start gap-1">
                         <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="false">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blood-800 dark:ring-offset-blood text-blood">
                         </div>
                         <div class="ml-3 text-sm">
                             <label for="remember" class="text-gray-500 dark:text-tome mr-2 md:mr-0">Remember me</label>
@@ -65,9 +67,9 @@
                     </div>
                     <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-scroll-900">Forgot password?</a>
                 </div>
-                <button @click="signIn" type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <button @click="signUp" type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                 <p class="text-sm font-light text-gray-500 dark:text-tome-900">
-                    Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-tome-900">Sign up</a>
+                    Don’t have an account yet? <NuxtLink to="signUp" class="font-medium text-primary-600 hover:underline dark:text-tome-900">Sign up</NuxtLink>
                 </p>
             </form>
         </div>
