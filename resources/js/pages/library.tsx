3bc1b03f6f8786918047b76f6184a2706c6d6178
Joel Christensen <christensenjo14@gmail.com>
2025-08-20 00:48:13 -0600

    Rename font-sans to font-serif for Dutch Med

--- resources/js/pages/library.tsx
+++ resources/js/pages/library.tsx
@@ -7,7 +7,7 @@ export default function Library() {
 			<Head title="Home" />
 			<PublicLayout>
 				<div className="flex flex-col items-center justify-center max-w-screen-lg mx-auto px-4 py-12">
-					<h1 className="text-5xl text-white font-sans mb-8 text-center">Library...</h1>
+					<h1 className="text-5xl text-white font-serif mb-8 text-center">Library...</h1>
 					<div className="w-full">
 						<p className="mb-6 leading-relaxed text-lg text-white/90">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut maximus enim, vitae facilisis erat. Curabitur volutpat ut turpis in hendrerit. Aenean interdum congue dui, suscipit commodo purus pellentesque porttitor. Sed euismod egestas nulla, quis gravida arcu malesuada eu. Etiam egestas tellus enim, eget faucibus purus blandit id. Integer ultricies, mi in interdum aliquet, erat lacus commodo nisl, eu porttitor lacus lacus non ex. Morbi molestie purus eget erat laoreet varius.
